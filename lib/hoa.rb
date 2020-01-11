ee8af18b3f1064b79e218dd9707a889ca8d952fe
Rick Glascock <rixong@gmail.com>
2020-01-11 15:19:31 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -7,8 +7,8 @@ BASE_HOA = {
 
 def add_character(show, name)
   
-  show<<name
-  return show
+  [show]<<name
+  return [show]
   
   # Write your implementation here
   # Should return the array of the 'show' argument
